a70cceae5535ce1ddf377aac11c427a5786f49e3
thatOneSnip3r <77989925+thatOneSnip3r@users.noreply.github.com>
2021-06-04 08:51:48 -0400

    -Final push
    It probably isn't tbh

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Score : MonoBehaviour
+{
+
+    public string textValue;
+    public Text textElement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
